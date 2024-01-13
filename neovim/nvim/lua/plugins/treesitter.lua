1a5fa1df977c4ebe639983ef8be6f39992b4193a
Cubball <138378823+Cubball@users.noreply.github.com>
2024-01-13 22:12:53 +0200

    Enable treesitter incremental highlighting

--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,16 @@ return {
             enable = true,
             additional_vim_regex_highlighting = false,
         },
+        incremental_selection = {
+            enable = true,
+            -- TODO: choose better mappings
+            keymaps = {
+                init_selection = "<CR>",
+                node_incremental = "<CR>",
+                scope_incremental = "<TAB>",
+                node_decremental = "<S-TAB>",
+            },
+        },
     },
     main = "nvim-treesitter.configs",
     init = function()
