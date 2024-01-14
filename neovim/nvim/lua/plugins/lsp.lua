326c34ff9be0c0ce8a235fa294b034defa6ab90e
Cubball <138378823+Cubball@users.noreply.github.com>
2024-01-14 22:48:35 +0200

    Clean up lsp config, remove now unnecessary lsp tokens fix for omnisharp

--- neovim/nvim/lua/plugins/lsp.lua
+++ neovim/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,14 @@
-function set_mappings()
-    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { desc = "[D]iagnostic [k] - previous" })
-    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { desc = "[D]iagnostic [j] - next" })
-    vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, { desc = "[D]iagnostic [O]pen" })
-    vim.keymap.set({ "n", "v" }, "<leader>rf", vim.lsp.buf.code_action, { desc = "[R]e[F]actor (code action)" })
-    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[G]o to [D]efinition" })
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[N]ame" })
+function set_mappings(client, buffer)
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { desc = "[D]iagnostic [k] - previous", buffer = buffer })
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { desc = "[D]iagnostic [j] - next", buffer = buffer })
+    vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, { desc = "[D]iagnostic [O]pen", buffer = buffer })
+    vim.keymap.set({ "n", "v" }, "<leader>rf", vim.lsp.buf.code_action, { desc = "[R]e[F]actor (code action)", buffer = buffer })
+    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[G]o to [D]efinition", buffer = buffer })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[N]ame", buffer = buffer })
     vim.keymap.set("n", "<leader>fm", function()
         vim.lsp.buf.format({ async = true })
-    end, { desc = "[F]or[M]at" })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "K - show info, help" })
-end
-
-function set_signature_help_mapping(client)
+    end, { desc = "[F]or[M]at", buffer = buffer })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "K - show info, help", buffer = buffer })
     if client.server_capabilities.signatureHelpProvider then
         require("lsp-overloads").setup(client, {
             display_automatically = false,
@@ -19,9 +16,9 @@ function set_signature_help_mapping(client)
                 border = { " ", "", " ", " ", " ", "", " ", " " },
             },
         })
-        vim.keymap.set({ "n", "i" }, "<C-n>", "<cmd>LspOverloadsSignature<CR>", { desc = "Ctrl + n - show signature help with overloads (if they are present)" })
+        vim.keymap.set({ "n", "i" }, "<C-n>", "<cmd>LspOverloadsSignature<CR>", { desc = "Ctrl + n - show signature help with overloads (if they are present)", buffer = buffer })
     else
-        vim.keymap.set({ "n", "i" }, "<C-n>", vim.lsp.buf.signature_help, { desc = "Ctrl + n - show signature help"})
+        vim.keymap.set({ "n", "i" }, "<C-n>", vim.lsp.buf.signature_help, { desc = "Ctrl + n - show signature help", buffer = buffer })
     end
 end
 
@@ -40,100 +37,22 @@ function c_sharp_highlighting()
     vim.api.nvim_set_hl(0, "@lsp.type.property_name", { link = "@field" })
 end
 
-function c_sharp_tokens_fix(client)
-    if client.name == "omnisharp" then
-        client.server_capabilities.semanticTokensProvider = {
-            full = vim.empty_dict(),
-            legend = {
-                tokenModifiers = { "static_symbol" },
-                tokenTypes = {
-                    "comment",
-                    "excluded_code",
-                    "identifier",
-                    "keyword",
-                    "keyword_control",
-                    "number",
-                    "operator",
-                    "operator_overloaded",
-                    "preprocessor_keyword",
-                    "string",
-                    "whitespace",
-                    "text",
-                    "static_symbol",
-                    "preprocessor_text",
-                    "punctuation",
-                    "string_verbatim",
-                    "string_escape_character",
-                    "class_name",
-                    "delegate_name",
-                    "enum_name",
-                    "interface_name",
-                    "module_name",
-                    "struct_name",
-                    "type_parameter_name",
-                    "field_name",
-                    "enum_member_name",
-                    "constant_name",
-                    "local_name",
-                    "parameter_name",
-                    "method_name",
-                    "extension_method_name",
-                    "property_name",
-                    "event_name",
-                    "namespace_name",
-                    "label_name",
-                    "xml_doc_comment_attribute_name",
-                    "xml_doc_comment_attribute_quotes",
-                    "xml_doc_comment_attribute_value",
-                    "xml_doc_comment_cdata_section",
-                    "xml_doc_comment_comment",
-                    "xml_doc_comment_delimiter",
-                    "xml_doc_comment_entity_reference",
-                    "xml_doc_comment_name",
-                    "xml_doc_comment_processing_instruction",
-                    "xml_doc_comment_text",
-                    "xml_literal_attribute_name",
-                    "xml_literal_attribute_quotes",
-                    "xml_literal_attribute_value",
-                    "xml_literal_cdata_section",
-                    "xml_literal_comment",
-                    "xml_literal_delimiter",
-                    "xml_literal_embedded_expression",
-                    "xml_literal_entity_reference",
-                    "xml_literal_name",
-                    "xml_literal_processing_instruction",
-                    "xml_literal_text",
-                    "regex_comment",
-                    "regex_character_class",
-                    "regex_anchor",
-                    "regex_quantifier",
-                    "regex_grouping",
-                    "regex_alternation",
-                    "regex_text",
-                    "regex_self_escaped_character",
-                    "regex_other_escape",
-                },
-            },
-            range = true,
-        }
-    end
-end
-
 function setup_servers(lsp)
+    local lsp = require("lspconfig")
     local local_app_data = os.getenv("LOCALAPPDATA")
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     lsp.html.setup({
         capabilities = capabilities,
+        on_attach = set_mappings,
     })
     lsp.cssls.setup({
         capabilities = capabilities,
+        on_attach = set_mappings,
     })
 
     lsp.tsserver.setup({
-        on_attach = function(client, _)
-            set_signature_help_mapping(client)
-        end,
+        on_attach = set_mappings,
     })
 
     lsp.omnisharp.setup({
@@ -146,23 +65,9 @@ function setup_servers(lsp)
         organize_imports_on_format = true,
         on_attach = function(client, buffer) 
             c_sharp_highlighting()
-            c_sharp_tokens_fix(client)
-            set_signature_help_mapping(client)
+            set_mappings(client, buffer)
         end,
     })
-
-    -- local configs = require("lspconfig.configs")
-    -- if not configs.rzls then
-    --     configs.rzls = {
-    --         default_config = {
-    --             cmd = { local_app_data .. "/rzls/rzls.exe" },
-    --             root_dir = lsp.util.root_pattern("*.sln", "*.csproj", "omnisharp.json", "function.json"),
-    --             filetypes = { "cshtml" },
-    --         },
-    --     }
-    -- end
-    --
-    -- lsp.rzls.setup({})
 end
 
 return {
@@ -171,9 +76,5 @@ return {
         "Hoffs/omnisharp-extended-lsp.nvim",
         "Issafalcon/lsp-overloads.nvim",
     },
-    config = function()
-        set_mappings()
-        local lsp = require("lspconfig")
-        setup_servers(lsp)
-    end,
+    config = setup_servers,
 }
