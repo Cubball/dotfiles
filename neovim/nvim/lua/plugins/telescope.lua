6e66714973e2a6d63d888960976545bc8271070e
Cubball <138378823+Cubball@users.noreply.github.com>
2024-03-19 14:59:25 +0200

    Add new mappings to telescope

--- neovim/nvim/lua/plugins/telescope.lua
+++ neovim/nvim/lua/plugins/telescope.lua
@@ -10,10 +10,16 @@ return {
         { "<leader>fw", function() require("telescope.builtin").grep_string() end, desc = "[F]ind [W]ord (under cursor)" },
         { "<leader>fc", function() require("telescope.builtin").live_grep() end, desc = "[F]ind [C]ontents" },
         { "<leader>fg", function() require("telescope.builtin").git_files() end, desc = "[F]ind in [G]it" },
+        { "<leader>fb", function()
+            local builtin = require("telescope.builtin")
+            local themes = require("telescope.themes")
+            builtin.current_buffer_fuzzy_find(themes.get_dropdown())
+        end, desc = "[F]ind in [B]uffer" },
         { "<leader>da", function() require("telescope.builtin").diagnostics() end, desc = "[D]iagnostics [A]ll" },
         { "<leader>gi", function() require("telescope.builtin").lsp_implementations() end, desc = "[G]o to [I]mplementations" },
         { "<leader>gr", function() require("telescope.builtin").lsp_references() end, desc = "[G]o to [R]eferences" },
-        { "<leader>cs", function() require("telescope.builtin").colorscheme() end, desc = "[C]olor[S]cheme" },
+        { "<leader>fsd", function() require("telescope.builtin").lsp_document_symbols() end, desc = "[F]ind [S]ymbols in [D]ocument" },
+        { "<leader>fsw", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end, desc = "[F]ind [S]ymbols in [W]orkspace" },
     },
     config = function()
         local telescope = require("telescope")
