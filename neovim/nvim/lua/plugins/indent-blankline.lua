30ccf9a1809a13f001d510d98788bee58c91b0ff
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-09 12:23:08 +0300

    Remove custom commands to toggle indent-blankline, since it has them built in

--- neovim/nvim/lua/plugins/indent-blankline.lua
+++ neovim/nvim/lua/plugins/indent-blankline.lua
@@ -1,12 +1,8 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    init = function()
-        local ibl = require("ibl")
-        local indent_blankline_enabled = false
-        vim.api.nvim_create_user_command("IndentBlankLineToggle", function()
-            indent_blankline_enabled = not indent_blankline_enabled
-            ibl.setup({ enabled = indent_blankline_enabled })
-        end,
-        { })
-    end
+    event = "VeryLazy",
+    main = "ibl",
+    opts = {
+        enabled = false,
+    },
 }
