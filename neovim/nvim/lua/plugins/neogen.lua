3ba5b3fe1a4616432c24e984201714ab205a1539
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-14 11:54:08 +0300

    Add neogen

---
+++ neovim/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,14 @@
+return {
+    "danymat/neogen",
+    opts = {
+        languages = {
+            cs = {
+                template = {
+                    annotation_convention = "xmldoc",
+                },
+            },
+        },
+        snippet_engine = "luasnip",
+    },
+    cmd = "Neogen",
+}
