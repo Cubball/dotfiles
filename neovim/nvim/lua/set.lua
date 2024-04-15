a52509638c19695633dad98284b19629e181603c
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-15 17:14:46 +0300

    Fix options in set.lua

--- neovim/nvim/lua/set.lua
+++ neovim/nvim/lua/set.lua
@@ -1,29 +1,34 @@
 local o = vim.opt
-o.clipboard = "unnamed"
-o.expandtab = true
+-- UI
 o.fileencodings = "utf-8,cp1251"
 o.fillchars = { eob = " " }
 o.guicursor = ""
 o.hlsearch = false
-o.ignorecase = true
-o.iminsert = 0
-o.imsearch = -1
 o.incsearch = true
-o.keymap = "ukrainian-jcuken-improved"
 o.nu = true
 o.relativenumber = true
 o.scrolloff = 8
+o.termguicolors = true
+o.wrap = false
+
+-- Input
+o.clipboard = "unnamed"
+o.keymap = "ukrainian-jcuken-improved"
+o.iminsert = 0
+o.imsearch = -1
+o.expandtab = true
 o.shiftwidth = 4
-o.smartcase = true
-o.smartindent = true
 o.softtabstop = 4
+o.tabstop = 4
+o.smartindent = true
+o.ignorecase = true
+o.smartcase = true
+o.timeoutlen = 500
+
+-- Other
 o.splitbelow = true
 o.splitright = true
 o.swapfile = false
-o.tabstop = 4
-o.termguicolors = true
-o.timeoutlen = 500
-o.wrap = false
 
 local yank_highlight_group = vim.api.nvim_create_augroup("yank_highlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
