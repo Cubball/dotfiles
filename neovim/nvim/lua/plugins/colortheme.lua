04c1bd680afefc975e37ac73fa5ab8ac16745532
Cubball <138378823+Cubball@users.noreply.github.com>
2026-02-08 10:54:54 +0200

    chore: Remove unused colorthemes, update terminal color

--- neovim/nvim/lua/plugins/colortheme.lua
+++ neovim/nvim/lua/plugins/colortheme.lua
@@ -1,42 +1,5 @@
 return {
     {
-        "navarasu/onedark.nvim",
-        opts = {
-            style = "cool",
-            transparent = true,
-            term_colors = true,
-            code_style = {
-                comments = "none",
-                keywords = "none",
-                functions = "none",
-                strings = "none",
-                variables = "none",
-            },
-            highlights = {
-                ["@boolean"] = { fg = "$purple" },
-                ["@type.builtin"] = { fg = "$purple" },
-                ["@type.qualifier.typescript"] = { fg = "$purple" },
-                ["@tag.delimiter"] = { fg = "$fg" },
-                ["@lsp.type.property"] = { fg = "$fg" },
-                ["@lsp.type.variable"] = { fg = "$red" },
-                ["@lsp.type.fieldName"] = { fg = "$fg" },
-                ["@lsp.type.enumMember"] = { fg = "$fg" },
-                ["@lsp.type.constantName"] = { fg = "$fg" },
-            },
-        },
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        main = "rose-pine",
-        opts = {
-            styles = {
-                italic = false,
-                transparency = true,
-            },
-        },
-    },
-    {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
@@ -81,21 +44,4 @@ return {
             vim.cmd[[colorscheme tokyonight]]
         end,
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        opts = {
-            no_italic = true,
-            integrations = {
-                telescope = {
-                    enabled = true,
-                    style = "nvchad",
-                },
-            },
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            -- vim.cmd[[colorscheme catppuccin-mocha]]
-        end,
-    }
 }
